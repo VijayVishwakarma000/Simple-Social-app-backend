b50cbed9dc51fd485bac283e5518e1970f0dc631
Vijay Vishwakarma <76258542+VijayVishwakarma000@users.noreply.github.com>
2026-03-25 18:31:54 +0530

    Update authmodel.js

--- models/authmodel.js
+++ models/authmodel.js
@@ -37,7 +37,7 @@ const crypto = require("crypto");
       createdAt: new Date(),
     });
 
-    return { code: 0, msg: "User created successfully" };
+    return { code: 0, msg: "User created successfully",err:error.message,stack:error.stack };
   } catch (error) {
     return { code: -500, msg: "Server error" };
   }
