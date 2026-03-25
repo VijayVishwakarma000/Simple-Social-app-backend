a9ea40855e32d3341235200cb3d5f1cb7581da01
Vijay Vishwakarma <76258542+VijayVishwakarma000@users.noreply.github.com>
2026-03-25 17:40:53 +0530

    Improve error response structure in user creation
    Refine error handling in user creation process.

--- models/authmodel.js
+++ models/authmodel.js
@@ -38,8 +38,9 @@ const crypto = require("crypto");
     });
 
     return { code: 0, msg: "User created successfully" };
-  } catch (error) {
-    return { code: -500, msg: "Server error",err:error };
+  } catch (err) {
+    return { code: -500, msg: "Server error", error: err.message,
+  stack: err.stack,};
   }
 }
 
